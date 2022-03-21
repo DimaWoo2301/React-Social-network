9ade56479eb5fb60ccd20f86785258379c9b55a8
DimaWoo <DimaWoo2301@gmail.com>
2022-03-21 21:23:17 +0300

    added WitchRouter for ProfileContainer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <div className="container">
         <div className="wrapper_hero">
           <NavbarContainer />
-          <Route path="/profile" render={() => <ProfileContainer />} />
+          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
           <Route path="/messages" render={() => <DialogsContainer />} />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import Profile from "./Profile";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUsersProfile } from "../../Redax/profile-reducer";
+import { withRouter } from "react-router-dom";
 
 export class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUsersProfile(response.data);
       });
@@ -20,4 +25,9 @@ export class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
-export default connect(mapStateToProps, { setUsersProfile })(ProfileContainer);
+
+let WitchUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUsersProfile })(
+  WitchUrlDataContainerComponent
+);
